cc84d19af5fe342613aa4857b68d7351c2bf58ee
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2019-10-30 20:57:55 +0530

    Update wp-load.php

--- wp-load.php
+++ wp-load.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Bootstrap file for setting the ABSPATH constant
+ comments
  * and loading the wp-config.php file. The wp-config.php
  * file will then load the wp-settings.php file, which
  * will then set up the WordPress environment.
